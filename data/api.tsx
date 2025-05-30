827682cc1f25a611d1f613530665873c462e9418
nazerkerysbekova1 <nazerkerysbek32@gmail.com>
2025-05-30 16:17:10 +0500

    api url

--- data/api.tsx
+++ data/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect } from 'react';
 
-const API_URL = 'http://192.168.172.105:4000/api';
+// const API_URL = 'http://192.168.172.105:4000/api';
 
 export const fetchData = async (endpoint: string): Promise<any> => {
   const headers: HeadersInit = {
